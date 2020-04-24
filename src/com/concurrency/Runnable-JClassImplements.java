0f96c81451da201b254031666840fcff340ec4cc
Yücel Yılmaz <btr316530@gmail.com>
2020-04-25 00:41:14 +0300

    The other way to specify what code a thread should run is by creating a class that implements the java.lang.Runnable interface. A Java object that implements the Runnable interface can be executed by a Java Thread.

---
+++ src/com/concurrency/Runnable-JClassImplements.java
@@ -0,0 +1,15 @@
+package com.concurrency;
+
+class MyRunnable implements java.lang.Runnable {
+    @Override
+    public void run() {
+        System.out.println("Runnable is running.");
+    }
+}
+
+class RunnableInterface {
+    public static void main(String[] args) {
+        MyRunnable myRunnable = new MyRunnable();
+        myRunnable.run();
+    }
+}
